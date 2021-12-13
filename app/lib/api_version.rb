8b8b6149c4c34331091bd189e602a0affc992dc1
alv2r <alv.rsl@gmail.com>
2021-12-12 23:53:05 -0500

    Adds mime API versioning

---
+++ app/lib/api_version.rb
@@ -0,0 +1,21 @@
+class ApiVersion
+  attr_reader :version, :default
+
+  def initialize(version, default = false)
+    @version = version
+    @default = default
+  end
+
+  # Check whether version is specified or is default
+  def matches?(request)
+    check_headers(request.headers) || default
+  end
+
+  private
+
+  # Check version from Accept headers
+  def check_headers(headers)
+    accept = headers[:accept]
+    accept&.include?("application/vnd.todos.#{version}+json")
+  end
+end
